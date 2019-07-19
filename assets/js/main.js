e459f59a55ebf6df1950a2f4771f50cc8ac5a00c
Bruno Bincoletto <bruno@bedigital.com.br>
2019-07-19 12:47:04 -0300

    ...

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-// Mensagem ao desenvolvedor que irá validar este teste.
-
-// Deixei o ambiente corporativo em 2013 e desde então trabalho como autonomo com plataformas em php. Devido a isso deixei de me atualizar com algumas tecnologias. Perdi um pouco da onda de NodeJS, ES5, etc. vi poucas coisas e o pouco que vi apliquei aqui.
-// Porém minha experiência com frontend e ux é enorme, aprendo com muita facilidade e se me permitirem a chance aprendo as novas tecnologias rápidinho.
-// Não sei exatamente com qual framework a Venturus trabalha, mas por citarem "Vanilla JS" imagino que com todos.
-// Obs: evitei o uso do jQuery, mas tenho grande experiência com o mesmo.
 
 new Vue({
   el: '#app',
